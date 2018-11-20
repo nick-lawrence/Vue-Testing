a30eb46d7034ad8ee40bdad6a7d0a72b62df16c2
nick-lawrence <nicklawrence.design@gmail.com>
2018-11-20 06:14:41 -0800

    added vue client-side subfolder

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const path = require("path");
 
+
+//Import typeDefs and resolvers
 const filePath = path.join(__dirname, "typeDefs.gql");
 const typeDefs = fs.readFileSync(filePath, "utf-8");
 const resolvers = require("./resolvers");
 
+//Import Environment variables and Mongoose Models
 require("dotenv").config({ path: "variables.env" });
 const User = require("./models/User");
 const Post = require("./models/Post");
 
+//Connect to MLab Database
 mongoose
   .connect(
     process.env.MONGO_URI,
@@ -19,6 +23,7 @@ mongoose
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
 
+// Create Apollo/GraphQL Server using typeDefs, resolvers, and context object
 const server = new ApolloServer({
   typeDefs,
   resolvers,
